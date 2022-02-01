ddc16a8908565741b2e6867c2de53333ee3dae29
iurylemos <iurylemos10@gmail.com>
2022-01-31 23:36:27 -0300

    ensure LocalSavePurchases does not delete cache upon creation

--- src/domain/data/usecases/localSavePurchases.spec.ts
+++ src/domain/data/usecases/localSavePurchases.spec.ts
@@ -1,3 +1,17 @@
+class LocalSavePurchases {
+  constructor(private readonly cacheStore: CacheStore) {}
+}
+
+interface CacheStore {}
+
+class CacheStoreSpy implements CacheStore {
+  deleteCallsCount = 0;
+}
+
 describe("Local Save Purchases", () => {
-  it("should return the value right", () => {});
+  it("should not delete cache on sut.init", () => {
+    const cacheStore = new CacheStoreSpy();
+    new LocalSavePurchases(cacheStore);
+    expect(cacheStore.deleteCallsCount).toBe(0);
+  });
 });
