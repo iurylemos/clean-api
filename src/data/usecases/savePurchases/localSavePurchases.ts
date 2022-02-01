b9a676dfbd12c24e4ee9e58b20c6ccbb8a9ecf0f
iurylemos <iurylemos10@gmail.com>
2022-02-01 01:13:56 -0300

    change index for domain usecases folder

--- src/data/usecases/savePurchases/localSavePurchases.ts
+++ src/data/usecases/savePurchases/localSavePurchases.ts
@@ -1,8 +1,10 @@
 import { CacheStore } from "@/data/protocols/cache";
-import { SavePurchases } from "@/domain";
+import { SavePurchases } from "@/domain/usecases";
 
 export class LocalSavePurchases implements SavePurchases {
-  constructor(private readonly cacheStore: CacheStore) {}
+  constructor(
+    private readonly cacheStore: CacheStore<SavePurchases.Params[]>
+  ) {}
 
   async save(purchases: SavePurchases.Params[]): Promise<void> {
     this.cacheStore.delete("purchases");
